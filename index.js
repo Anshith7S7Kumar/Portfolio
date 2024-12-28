427d7e1817e58c8ecd8650cadc4e4333e3eb0c0c
Anshith7S7Kumar <anshith.s@gmail.com>
2024-12-28 15:28:32 +0530

    Vercel

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
